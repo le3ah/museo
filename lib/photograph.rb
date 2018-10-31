6602bb10f6e5a9b68269ea8909df69afed9a6a9c
Leah Miller <lkmiller.87@gmail.com>
2018-10-31 09:15:25 -0600

    Add passing tests for initialize

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,4 +1,5 @@
 class Photograph
+  attr_reader :id, :name, :artist_id, :year 
   def initialize(attributes)
     @id = attributes[:id]
     @name = attributes[:name]
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -13,4 +13,48 @@ class PhotographTest<Minitest::Test
     photograph = Photograph.new(attributes)
     assert_instance_of Photograph, photograph
   end
+
+  def test_it_has_id
+    attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "4",
+      year: "1954"
+                  }
+    photograph = Photograph.new(attributes)
+    assert_equal "1", photograph.id
+  end
+
+  def test_it_has_a_name
+    attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "4",
+      year: "1954"
+                  }
+    photograph = Photograph.new(attributes)
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", photograph.name
+  end
+
+  def test_it_has_artist_id
+    attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "4",
+      year: "1954"
+                  }
+    photograph = Photograph.new(attributes)
+    assert_equal "4", photograph.artist_id
+  end
+
+  def test_it_has_a_year
+    attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "4",
+      year: "1954"
+                  }
+    photograph = Photograph.new(attributes)
+    assert_equal "1954", photograph.year 
+  end
 end
