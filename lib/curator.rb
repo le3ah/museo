6223f85ea6f2ef438e219d3b725ebdf6dc2a6f87
Leah Miller <lkmiller.87@gmail.com>
2018-10-31 09:31:56 -0600

    Add passing test for adding photographs to curator

--- lib/curator.rb
+++ lib/curator.rb
@@ -4,4 +4,8 @@ class Curator
     @artists = []
     @photographs = []
   end
+
+  def add_photograph(photo)
+    @photographs << photo 
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/curator'
+require './lib/photograph'
 
 class CuratorTest < Minitest::Test
   def test_it_exists
@@ -16,5 +17,24 @@ class CuratorTest < Minitest::Test
   def test_it_starts_with_zero_photographs
     curator = Curator.new
     assert_equal [], curator.photographs
+  end
+
+  def test_curator_can_add_photographs
+    curator = Curator.new
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+              }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+              }
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    assert_equal 2, curator.photographs.count
   end
 end
